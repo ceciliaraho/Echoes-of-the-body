98e4d27126e948543f15754fd6a69ace708ba838
Cecilia Raho <118004048+ceciliaraho@users.noreply.github.com>
2025-07-06 19:01:54 +0200

    Update random_forest_classifier.py

--- model/random_forest_classifier.py
+++ model/random_forest_classifier.py
@@ -6,31 +6,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 
-# === 1. Caricamento dati ===
+# Load data
 df = pd.read_csv("features_dataset_clean.csv")
 
-# === 2. Separazione X e y ===
+# Split data 
 X = df.drop(columns=["label", "participant", "time_center"])
 y = df["label"]
 
-# Encoding etichette
+# Encoding label
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
 
-# === 3. Train/test split ===
+# Train/test split 
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
 
-# === 4. Addestramento modello Random Forest ===
+# Random Forest
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
 
-# === 5. Predizione e valutazione ===
+# Accurancy and prediction
 y_pred = clf.predict(X_test)
-print("✅ Accuracy:", round(accuracy_score(y_test, y_pred) * 100, 2), "%")
-print("\n=== Classification Report ===")
+print("Accuracy:", round(accuracy_score(y_test, y_pred) * 100, 2), "%")
+print("Classification Report: ")
 print(classification_report(y_test, y_pred, target_names=le.classes_))
 
-# === 6. Confusion Matrix ===
+# Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm, annot=True, fmt='d', xticklabels=le.classes_, yticklabels=le.classes_, cmap="Blues")
@@ -40,7 +40,7 @@ plt.title("Confusion Matrix - Random Forest")
 plt.tight_layout()
 plt.show()
 
-# === 7. Feature Importance ===
+# Feature Importance
 importances = clf.feature_importances_
 feature_names = X.columns
 feat_df = pd.DataFrame({"Feature": feature_names, "Importance": importances})
