1b6c29dee6868b644557d7b84d4f257c04ba1885
Cecilia Raho <118004048+ceciliaraho@users.noreply.github.com>
2025-07-06 17:30:56 +0200

    Update labeling.py

--- analysis/labeling.py
+++ analysis/labeling.py
@@ -1,16 +1,5 @@
-# labeling.py
-
 def assign_labels(df, labels_dict):
-    """
-    Aggiunge una colonna 'label' al DataFrame in base ai secondi indicati nel dizionario.
-
-    Parametri:
-        df (pd.DataFrame): deve contenere la colonna 'time_from_start'
-        labels_dict (dict): chiavi = nome fase, valori = (start_time, end_time) in secondi
-
-    Ritorna:
-        df con colonna 'label'
-    """
+   
     df = df.copy()
     df['label'] = 'unlabeled'
 
